f7a462eeb39f25512162c1aab42e8afe27041f95
Andrea Spina <andrea.spina@radicalbit.io>
2017-03-12 19:04:28 +0100

    KeyedTimeEvent structure added

---
+++ src/main/scala/org/spixi/flink/handson/model/KeyedTimeEvent.scala
@@ -0,0 +1,10 @@
+package org.spixi.flink.handson.model
+
+
+case class KeyedTimeEvent[T](timestamp: Long, key: String, value: T) extends TimeEvent[T] {
+
+  override def toString: String = {
+    timestamp + "," + key + "," + value
+  }
+
+}
